0eba92345048e3a0a09afe02a469b65d34e0f270
IvanKurikin <ivan.kurykin@innowise.com>
2025-04-27 21:22:54 +0300

    update entities

--- InnoClinic.ProfilesAPI/Domain/Entities/Doctor.cs
+++ InnoClinic.ProfilesAPI/Domain/Entities/Doctor.cs
@@ -1,16 +1,11 @@
 ﻿namespace Domain.Entities;
 
-public sealed class Doctor
+public sealed class Doctor : Person
 {
-    public Guid Id { get; set; }
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;        
-    public string? MiddleName { get; set; }
     public required DateTime DateOfBirth { get; set; }
     public string Email { get; set; } = string.Empty;
     public string Specialization { get; set; } = string.Empty;
     public string Office { get; set; } = string.Empty;
     public string CareerStartYear { get; set; } = string.Empty;
     public string Status { get; set; } = "At work";
-    public string? PhotoUrl { get; set; }
 }
--- InnoClinic.ProfilesAPI/Domain/Entities/Patient.cs
+++ InnoClinic.ProfilesAPI/Domain/Entities/Patient.cs
@@ -1,13 +1,8 @@
 ﻿namespace Domain.Entities;
 
-public sealed class Patient
+public sealed class Patient : Person
 {
-    public Guid Id { get; set; }
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
-    public string? MiddleName { get; set; }
     public string PhoneNumber { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public required DateTime DateOfBirth { get; set; }
-    public string? PhotoUrl { get; set; } 
 }
